70ac86895419466c9db5b96ac969306cefdb61ce
anna_k <ackongs@gmail.com>
2022-07-05 10:37:48 -0600

    updated user info

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//profile info
+const overview = document.querySelector(".overview");
+const username = "bananaroo";
+
+const infoFetch = async function () {
+    const myInfo = await fetch (`https://api.github.com/users/${username}`);
+    const data = await myInfo.json();
+    displayInfo(data);
+};
+
+infoFetch();
+
+const displayInfo = function (data) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = `<figure>
+    <img alt="user avatar" src=${data.avatar_url} />
+  </figure>
+  <div>
+    <p><strong>Name:</strong> ${data.name}</p>
+    <p><strong>Bio:</strong> ${data.bio}</p>
+    <p><strong>Location:</strong> ${data.location}</p>
+    <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+  </div> `;
+  overview.append(div);
+};
